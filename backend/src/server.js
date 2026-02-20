b3528d125387395e1007d38b4d65bb5045764bb7
Mark Angelo Nool <markangelonool0@gmail.com>
2026-02-20 23:09:04 +0800

    updated server and the CSP warnings

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
 
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
@@ -11,7 +12,8 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //middleware
 if (process.env.NODE_ENV !== "production") {
@@ -41,10 +43,18 @@ if (process.env.NODE_ENV === "production") {
     next();
   });
 
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(
+      path.join(__dirname, "../../frontend/dist/index.html"),
+      (err) => {
+        if (err) {
+          console.error("Error sending index.html:", err);
+          res.status(500).send("Server error");
+        }
+      },
+    );
   });
 }
 connectDB().then(() => {
